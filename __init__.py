8e4259c0fdf461909f9499dbd67a29ec836ae767
Ethan Shernan <eshernan3@gatech.edu>
2015-01-23 14:53:00 -0500

    Adding framework from latex2onenote

--- __init__.py
+++ __init__.py
@@ -1,10 +1,65 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
+import requests, random, string
+import os
+from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    print('Hi')
-    return 'Hello World!'
+ALLOWED_EXTENSIONS = set(['java'])
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def home():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                  for x in xrange(32))
+    session['state'] = state
+    return render_template("index.html", state=state)
+
+@app.route("/process", methods=['POST'])
+def process_java():
+    #Make a randomly generated directory for latex compilation
+    staticFilepath = 'static/javaComp/' + random.choice(string.letters) + random.choice(string.letters) + random.choice(string.letters) + '/'
+
+    d = os.path.dirname(staticFilepath)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+    #If directory already exists, throw an error
+    else:
+        return render_template("error.html", message="Oh no D:! Something went wrong :( Please try again!")
+
+    file = request.files['java']
+    filename = ""
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(staticFilepath, filename))
+    else:
+        return render_template("error.html", message="Oh no D:! Something went wrong :( Please try again!")
+
+    #f = open(staticFilepath + '/latex.tex', 'w')
+    #f.write(latex_input)
+    #f.close()
+
+    #Compile LaTeX
+    bashCommand = "javac " + filename;
+    import subprocess
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, cwd=staticFilepath)
+    output = process.communicate()[0]
+
+    #f = open(staticFilepath + '/' + filename.rsplit('.', 1)[0] + '.html')
+    #finalHTML = f.read()
+
+    import shutil
+    shutil.rmtree(staticFilepath)
+
+    return render_template("success.html", compiler_msg=output)
+
+@app.route("/back", methods=['GET'])
+def back_to_input():
+    return render_template("index.html")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+app.secret_key = open('static/app_key.cfg', 'r+').read()
+if __name__ == "__main__":
+    app.run(debug="true")
